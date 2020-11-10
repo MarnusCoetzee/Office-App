717bc985f8c405a2735807252381b42352656964
MarnusCoetzee <marnuscoe@gmail.com>
2020-11-10 16:31:13 +0200

    added pwa service functionality

--- src/app/services/pwa.service.ts
+++ src/app/services/pwa.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
-
+import { ApplicationRef, Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { Observable, of, timer } from 'rxjs';
+import { catchError, first, mapTo, switchMap, timeout } from 'rxjs/operators';
+import { from as fromPromise } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PwaService {
+  constructor(private appRef: ApplicationRef, private swUpdate: SwUpdate) {
+    if (this.swUpdate.isEnabled) {
+      this.appRef.isStable
+        .pipe(
+          first((isStable) => isStable === true),
+          switchMap(() => this.swUpdate.available)
+        )
+        .subscribe(() => {
+          this.swUpdate.activateUpdate().then(() => document.location.reload());
+        });
+    }
+  }
 
-  constructor() { }
+  checkForUpdate(): Observable<boolean> {
+    const waitFor = 1000;
+
+    if (this.swUpdate.isEnabled) {
+      const available$ = this.swUpdate.available.pipe(
+        mapTo(true),
+        timeout(waitFor),
+        catchError(() => of(false))
+      );
+
+      return fromPromise(this.swUpdate.checkForUpdate()).pipe(
+        switchMap(() => available$)
+      );
+    }
+
+    return timer(waitFor).pipe(mapTo(false));
+  }
 }
--- src/app/shared/splash-screen/splash-screen.component.ts
+++ src/app/shared/splash-screen/splash-screen.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  animate,
+  animateChild,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  ApplicationRef,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { PwaService } from '../../services/pwa.service';
 @Component({
   selector: 'app-splash-screen',
-  templateUrl: './splash-screen.component.html',
-  styleUrls: ['./splash-screen.component.scss']
+  template: `
+    <div class="splash-screen" *ngIf="show" @fadeOut>
+      // Your custom splash screen design
+    </div>
+  `,
+  animations: [
+    // the fade-in/fade-out animation.
+    trigger('fadeOut', [
+      transition(':leave', [
+        query(':leave', animateChild(), { optional: true }),
+        animate(300, style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
+  styles: [
+    `
+      .splash-screen {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 9999;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SplashScreenComponent implements OnInit {
+  show: boolean = true;
 
-  constructor() { }
+  constructor(
+    private pwaService: PwaService,
+    private cdr: ChangeDetectorRef,
+    private appRef: ApplicationRef
+  ) {}
 
   ngOnInit(): void {
+    this.pwaService.checkForUpdate().subscribe((result) => {
+      this.show = result;
+      this.cdr.detectChanges();
+    });
   }
-
 }
